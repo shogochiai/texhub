05021ad638a421b20b9f133f9307840aa9af8020
SHUHEI HIYA <syuhei176@gmail.com>
2018-08-03 11:49:38 +0900

    return exit code when command was got error

--- index.js
+++ index.js
@@ -8,5 +8,18 @@ program
   .usage(`blankpaper (${Object.keys(command).join("|")})`)
   .parse(process.argv)
 
-if(command[program.args[0]]) command[program.args[0]]()
-else console.log('subcommand not found. %j is permitted', Object.keys(command))
+const cmd = program.args[0]
+if(command[cmd]) {
+  main(cmd)
+} else {
+  console.log('subcommand not found. %j is permitted', Object.keys(command))
+}
+
+async function main(cmd) {
+  try {
+    await command[cmd]()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
