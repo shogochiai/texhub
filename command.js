63cf33e5f849a8e3bc666eef06a99ecb1cefef6f
Sg <shogo1104@gmail.com>
2018-09-20 19:12:58 +0900

    remove node-latex because squash error message

--- command.js
+++ command.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
-const latex = require('node-latex')
 const LATEX_END = '\\end{document}'
 const fs = require('fs')
-const streamToPromise = require('stream-to-promise')
 const PDF_NAME = "whitepaper.pdf"
 const texDir = process.env.TEX_DIR || "tex"
 const translatorFile = process.env.TRANSLATORS_FILE_NAME
@@ -47,30 +45,18 @@ module.exports = {
         fs.writeFileSync('.bulk', bulk)
 
         // Bake PDF
-        const input = fs.createReadStream('.bulk')
-        const output = fs.createWriteStream(PDF_NAME)
-        const pdf = latex(input)
-        const result = await streamToPromise(pdf).catch(e=> console.error(e) )
-        fs.writeFileSync(PDF_NAME, result)
+        await cmd("pdfaltex .bulk").catch(e=> console.error(e) )
+        await cmd(`mv .pdf ${PDF_NAME}`).catch(e=> console.error(e) )
+        await cmd(`rm .bulk`).catch(e=> console.error(e) )
 
         // Logging
         console.log("Generating... \n")
         console.log("  > "+PDF_NAME+"\n")
-        return result
+        return true
     },
 
     setup: _=>{
-        let curl = run('curl -sL "https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh" | sh')
-        let childProcess = curl.childProcess
-        childProcess.stdout.on('data', function (data) {
-            console.log('stdout: ' + data.toString());
-        });
-
-        childProcess.stderr.on('data', function (data) {
-            console.error('stderr: ' + data.toString());
-        });
-
-        return curl
+        return cmd('curl -sL "https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh" | sh').catch(e=> console.error(e) )
     },
 
     template: async _=>{
@@ -87,20 +73,9 @@ module.exports = {
     },
 
     install: _=>{
-console.log(require("fs").readFileSync("Texfile").toString())
+        console.log(require("fs").readFileSync("Texfile").toString())
         let packages = require("fs").readFileSync("Texfile").toString().replace(/\n/g, " ")
-        let cmd = `tlmgr install ${packages}`
-        let install = run(cmd)
-        let childProcess = install.childProcess
-        childProcess.stdout.on('data', function (data) {
-            console.log('stdout: ' + data.toString());
-        });
-
-        childProcess.stderr.on('data', function (data) {
-            console.error('stderr: ' + data.toString());
-        });
-
-        return install
+        return cmd(`tlmgr install ${packages}`).catch(e=> console.error(e) )
     }
 }
 
@@ -110,4 +85,17 @@ function dump(err, stdout, stderr) {
 
 function run(cmd) {
   return spawn("sh", ["-c", cmd])
+}
+
+function cmd(c){
+  let _cmd = run(c + ' | sh')
+  let childProcess = _cmd.childProcess
+  childProcess.stdout.on('data', function (data) {
+      console.log('stdout: ' + data.toString());
+  });
+
+  childProcess.stderr.on('data', function (data) {
+      console.error('stderr: ' + data.toString());
+  });
+  return _cmd
 }
